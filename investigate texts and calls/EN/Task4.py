52bd6072966aa7c5084b28280a44be743900f7d5
ohnozzy <ohnozzy@gmail.com>
2018-02-23 15:14:22 +0800

    Task4.py

--- investigate texts and calls/EN/Task4.py
+++ investigate texts and calls/EN/Task4.py
@@ -2,12 +2,12 @@
 Intro to Python Lab 1, Task 4
 
 Complete each task in the file for that task. Submit the whole folder
-as a zip file or GitHub repo. 
+as a zip file or GitHub repo.
 Full submission instructions are available on the Lab Preparation page.
 """
 
 """
-Read file into texts and calls. 
+Read file into texts and calls.
 It's ok if you don't understand how to read files
 You will learn more about reading files in future lesson
 """
@@ -24,13 +24,30 @@ with open('calls.csv', 'r') as f:
 """
 TASK 4:
 The telephone company want to identify numbers that might be doing
-telephone marketing. Create a set of possible telemarketers: 
+telephone marketing. Create a set of possible telemarketers:
 these are numbers that make outgoing calls but never send texts,
 receive texts or receive incoming calls.
 
-Print a message: 
+Print a message:
 "These numbers could be telemarketers: "
 <list of numbers>
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
+
+outgoing_call_num = set()
+other_num = set()
+
+for text in texts:
+    other_num.add(text[0])
+    other_num.add(text[1])
+
+for call in calls:
+    outgoing_call_num.add(call[0])
+    other_num.add(call[1])
+
+telemarket_num = list(outgoing_call_num - other_num)
+telemarket_num.sort()
+print("These numbers could be telemarketers: ")
+for num in telemarket_num:
+    print(num)
 
